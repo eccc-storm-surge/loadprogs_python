0d1a2be30846c92223fd449afb857d15ce015161
Oleksandr (Sasha) Huziy <oleksandr.huziy@canada.ca>
2019-01-03 16:52:53 +0000

    update exp scripts

--- src/experiments/FC70E16V2.py
+++ src/experiments/FC70E16V2.py
@@ -4,5 +4,5 @@ from main import main
 
 
 if __name__ == '__main__':
-    # main(config_path=Path("configs/rdsps/forecast/FC70E16V2.cfg"))
+    main(config_path=Path("configs/rdsps/forecast/FC70E16V2.cfg"))
     main(config_path=Path("configs/rdsps/forecast/op_during_FC70E16V2.cfg"))
