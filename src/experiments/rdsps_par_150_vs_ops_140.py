6b9955cf2d9b386198a165ba1eb8ad77747f3964
Oleksandr (Sasha) Huziy <oleksandr.huziy@canada.ca>
2019-01-14 14:24:31 +0000

    update 150par vs 140 ops experiment

--- src/experiments/rdsps_par_150_vs_ops_140.py
+++ src/experiments/rdsps_par_150_vs_ops_140.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 from main import main
 
+
+do_pa = False
+
 if __name__ == '__main__':
-    main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_pa_ops_140.cfg"))
-    main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_pa_par_150.cfg"))
+    if do_pa:
+        main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_pa_ops_140.cfg"))
+        main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_pa_par_150.cfg"))
+
+    main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_fc_ops_140.cfg"))
+    main(config_path=Path("configs/CPOP_RDSPS_150_par_vs_RDSPS_140_ops/rdsps_fc_par_150.cfg"))
