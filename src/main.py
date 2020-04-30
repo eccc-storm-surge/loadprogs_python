9e7133edb1cd463313afdeccaf161bf6f9ebd63e
olh001 <oleksandr.huziy@canada.ca>
2020-04-30 04:43:03 +0000

    Refactor saving to txt

--- src/main.py
+++ src/main.py
@@ -202,131 +202,135 @@ def main(config_path: Path = None):
         msg = f"Could not find {mod_nomvar} in {mod_dir}, please check your data or load_progs config file.."
         raise IOError(msg)
 
-    with out_file.open("w") as fout:
-        mod_groups_by_station = model_points.groupby("station_id")
 
-        for k, g in mod_groups_by_station:
-            logger.debug(f"{k} ({type(k)}) => {g}")
+    mod_groups_by_station = model_points.groupby("station_id")
 
-        logger.debug(mod_groups_by_station.head())
-        for c in mod_groups_by_station:
-            logger.debug(c)
+    for k, g in mod_groups_by_station:
+        logger.debug(f"{k} ({type(k)}) => {g}")
 
-        # Dump corresponding obs and mod data into a file for scoring
-        for s in stations:
-            logger.debug(f"{s.station_id}, {type(s.station_id)}")
+    logger.debug(mod_groups_by_station.head())
+    for c in mod_groups_by_station:
+        logger.debug(c)
 
-            mod_data = mod_groups_by_station.get_group(s.station_id).copy()
+    # Dump corresponding obs and mod data into a file for scoring
+    for s in stations:
+        logger.debug(f"{s.station_id}, {type(s.station_id)}")
 
-            if detide_obs:
-                obs_data = s.get_detided_series(do_filtering=obs_do_filtering)
-            else:
-                # still remove the long-term mean
-                obs_data = s.data["twl"] - np.nanmean(s.data["twl"].values)
+        mod_data = mod_groups_by_station.get_group(s.station_id).copy()
 
-            # diags for detiding
-            if plot_detiding_diag and detide_obs:
-                msg = f"plotting timeseries for {s.station_id}"
-                logging.info(msg)
-                plot_ts_and_spectre(obs_data,
-                                    "{}_{}".format(config["label"], s.station_id),
-                                    img_dir=out_dir,
-                                    subplot_titles=None,
-                                    raw_data=s.data["twl-mean"],
-                                    tides=s.data["tides"],
-                                    sup_title=f"OBS : {s.name} ({s.station_id})")
+        if detide_obs:
+            obs_data = s.get_detided_series(do_filtering=obs_do_filtering)
+        else:
+            # still remove the long-term mean
+            obs_data = s.data["twl"] - np.nanmean(s.data["twl"].values)
 
-                s.ttidecon.classic_style(to_file=str(out_dir / f"{s.station_id}_obs_tides.csv"))
+        # diags for detiding
+        if plot_detiding_diag and detide_obs:
+            msg = f"plotting timeseries for {s.station_id}"
+            logging.info(msg)
+            plot_ts_and_spectre(obs_data,
+                                "{}_{}".format(config["label"], s.station_id),
+                                img_dir=out_dir,
+                                subplot_titles=None,
+                                raw_data=s.data["twl-mean"],
+                                tides=s.data["tides"],
+                                sup_title=f"OBS : {s.name} ({s.station_id})")
 
-            # deprecated
-            # mod_data[f"{s.station_id}_obs"] = obs_data[mod_data["time"]].values
+            s.ttidecon.classic_style(to_file=str(out_dir / f"{s.station_id}_obs_tides.csv"))
 
-            # Newer note (20190502): this is changed to be as in the MATLAB version of loadprogs, i.e. remove mean of
-            # the observed data in the aligned table
+        # deprecated
+        # mod_data[f"{s.station_id}_obs"] = obs_data[mod_data["time"]].values
 
-            # take the time mean over the same time points as the model (unless there is missing data)
-            # Note: some time points will be added to the mean few times in a similar way as the model has
-            # several values for the same time (with different lead times), this does not change the scores,
-            # but makes the scatter plots
-            # obs_data = obs_data.reindex(mod_data["time"])
+        # Newer note (20190502): this is changed to be as in the MATLAB version of loadprogs, i.e. remove mean of
+        # the observed data in the aligned table
 
-            # remove the mean over the current period from the data (to be more consistent with mod)
-            # if obs_remove_anal_period_mean:
-            #    obs_data -= obs_data.mean(skipna=True, axis=0)
-            #
+        # take the time mean over the same time points as the model (unless there is missing data)
+        # Note: some time points will be added to the mean few times in a similar way as the model has
+        # several values for the same time (with different lead times), this does not change the scores,
+        # but makes the scatter plots
+        # obs_data = obs_data.reindex(mod_data["time"])
 
-            # detide model time series if requested
-            if detide_mod:
-                logger.info("Detiding model outputs.")
-                assert not any(mod_data["time"].isna())
+        # remove the mean over the current period from the data (to be more consistent with mod)
+        # if obs_remove_anal_period_mean:
+        #    obs_data -= obs_data.mean(skipna=True, axis=0)
+        #
 
-                # for b2b operations
-                select_crit = mod_data["valid_hour"] <= b2b_freq_hours
-                select_crit = select_crit & (mod_data["valid_hour"] > 0) # remove t=0
-                mod_data_twl = mod_data.loc[select_crit, :]
-                mod_data_twl.sort_values("time", inplace=True)
-                logger.debug(mod_data_twl.head())
+        # detide model time series if requested
+        if detide_mod:
+            logger.info("Detiding model outputs.")
+            assert not any(mod_data["time"].isna())
 
-                mod_data_twl.set_index("time", inplace=True)
+            # for b2b operations
+            select_crit = mod_data["valid_hour"] <= b2b_freq_hours
+            select_crit = select_crit & (mod_data["valid_hour"] > 0) # remove t=0
+            mod_data_twl = mod_data.loc[select_crit, :]
+            mod_data_twl.sort_values("time", inplace=True)
+            logger.debug(mod_data_twl.head())
 
-                for c in mod_member_keys:
-                    mod_tides, mod_to_filter, mod_ttide_con = obs.get_tides_and_filter_hourly(
-                        mod_data_twl.loc[:, c].to_frame(), constituents=detide_mod_constituents)
+            mod_data_twl.set_index("time", inplace=True)
 
-                    # remove longterm mean
-                    mod_data.loc[:, c] -= mod_data_twl[c].mean()
-                    # detiding
-                    mod_data.loc[:, c] -= mod_tides.loc[mod_data["time"]].values
+            for c in mod_member_keys:
+                mod_tides, mod_to_filter, mod_ttide_con = obs.get_tides_and_filter_hourly(
+                    mod_data_twl.loc[:, c].to_frame(), constituents=detide_mod_constituents)
 
-                    # filtering
-                    if mod_do_filtering:
-                        mod_data.loc[:, c] -= mod_to_filter.loc[mod_data["time"]].values
+                # remove longterm mean
+                mod_data.loc[:, c] -= mod_data_twl[c].mean()
+                # detiding
+                mod_data.loc[:, c] -= mod_tides.loc[mod_data["time"]].values
 
-                    # diags for detiding
-                    if plot_detiding_diag:
-                        msg = f"plotting timeseries for mod at {s.station_id}"
-                        logging.info(msg)
+                # filtering
+                if mod_do_filtering:
+                    mod_data.loc[:, c] -= mod_to_filter.loc[mod_data["time"]].values
 
-                        plot_ts_and_spectre(
-                            mod_data_twl[c] - mod_data_twl[c].mean() - mod_tides.loc[mod_data_twl.index] -
-                            mod_to_filter.loc[mod_data_twl.index],
-                            "mod_{}_{}".format(config["label"], s.station_id),
-                            img_dir=out_dir,
-                            subplot_titles=None,
-                            raw_data=mod_data_twl[c] - mod_data_twl[c].mean(),
-                            tides=mod_tides, sup_title=config["label"].upper() + f": {s.name} ({s.station_id})")
+                # diags for detiding
+                if plot_detiding_diag:
+                    msg = f"plotting timeseries for mod at {s.station_id}"
+                    logging.info(msg)
 
-                        mod_ttide_con.classic_style(to_file=str(out_dir / f"{s.station_id}_mod_tides.csv"))
+                    plot_ts_and_spectre(
+                        mod_data_twl[c] - mod_data_twl[c].mean() - mod_tides.loc[mod_data_twl.index] -
+                        mod_to_filter.loc[mod_data_twl.index],
+                        "mod_{}_{}".format(config["label"], s.station_id),
+                        img_dir=out_dir,
+                        subplot_titles=None,
+                        raw_data=mod_data_twl[c] - mod_data_twl[c].mean(),
+                        tides=mod_tides, sup_title=config["label"].upper() + f": {s.name} ({s.station_id})")
 
-            # align model and observation timeseries in time
-            mod_data.loc[:, f"{s.station_id}_obs"] = obs_data[mod_data["time"]].values
+                    mod_ttide_con.classic_style(to_file=str(out_dir / f"{s.station_id}_mod_tides.csv"))
 
-            mod_data.dropna(inplace=True)
+        # align model and observation timeseries in time
+        mod_data.loc[:, f"{s.station_id}_obs"] = obs_data[mod_data["time"]].values
 
-            if remove_anal_period_mean:
-                tmean = mod_data.loc[(mod_data["valid_hour"] <= b2b_freq_hours) & (mod_data["valid_hour"] > 0), f"{s.station_id}_obs"].mean()
-                mod_data.loc[:, f"{s.station_id}_obs"] -= tmean
+        mod_data.dropna(inplace=True)
 
-                logger.debug(f"tmean({s.station_id})={tmean}")
+        if remove_anal_period_mean:
+            tmean = mod_data.loc[(mod_data["valid_hour"] <= b2b_freq_hours) & (mod_data["valid_hour"] > 0), f"{s.station_id}_obs"].mean()
+            mod_data.loc[:, f"{s.station_id}_obs"] -= tmean
 
-                # mean to be removed from each member calculated based on the control member, which is assumed
-                # to be the first in the list
+            logger.debug(f"tmean({s.station_id})={tmean}")
 
-                where_cond = (mod_data["valid_hour"] <= b2b_freq_hours) & (mod_data["valid_hour"] > 0)
-                tmean = mod_data.loc[where_cond, mod_member_keys[0]].mean()
+            # mean to be removed from each member calculated based on the control member, which is assumed
+            # to be the first in the list
 
-                for cn in mod_member_keys:
-                    mod_data.loc[:, cn] -= tmean  # remove long time mean only of the control member
+            where_cond = (mod_data["valid_hour"] <= b2b_freq_hours) & (mod_data["valid_hour"] > 0)
+            tmean = mod_data.loc[where_cond, mod_member_keys[0]].mean()
 
-            # select only runs run_freq_hours apart (usually it is 36h)
-            mod_data = mod_data.loc[mod_data["date_of_origin"].isin(origin_dates_of_interest), :]
+            for cn in mod_member_keys:
+                mod_data.loc[:, cn] -= tmean  # remove long time mean only of the control member
 
+        # select only runs run_freq_hours apart (usually it is 36h)
+        mod_data = mod_data.loc[mod_data["date_of_origin"].isin(origin_dates_of_interest), :]
+
+        if len(mod_data) > 0:
             rmse = np.linalg.norm(mod_data[f"{s.station_id}_obs"] - mod_data.loc[:, mod_member_keys].mean(axis=1)) / (
                 len(mod_data)) ** 0.5
             logger.debug(f"rmse({s.station_id})={rmse}")
 
-            logger.debug(f"{s.station_id}: found {len(mod_data[s.station_id + '_obs'])} corresponding data values")
+        logger.debug(f"{s.station_id}: found {len(mod_data[s.station_id + '_obs'])} corresponding data values")
 
+        logger.debug(f"Resulting dataframe:\n{mod_data.head()}")
+
+        with out_file.open("a") as fout:
             for row_index, row in mod_data.iterrows():
                 line = out_line_format.format(
                     int(row["valid_hour"]),
@@ -337,10 +341,10 @@ def main(config_path: Path = None):
                 )
                 fout.write(line)
 
-            if output_sql:
-                mod_data = mod_data.rename(columns={f"{s.station_id}_obs": "obs"})
-                conn = sqlite3.connect(out_dir / ("surge_" + config["label"] + ".sqlite"))
-                mod_data.to_sql(name="data", con=conn, index=False, if_exists='append')
+        if output_sql:
+            mod_data = mod_data.rename(columns={f"{s.station_id}_obs": "obs"})
+            conn = sqlite3.connect(out_dir / ("surge_" + config["label"] + ".sqlite"))
+            mod_data.to_sql(name="data", con=conn, index=False, if_exists='append')
 
     logger.info(f"Finished processing {config_path} .")
 
