4df30f1d6263e91e9536b878a2b602e84b1447b7
olh001 <oleksandr.huziy@canada.ca>
2020-05-29 20:10:44 +0000

    cleanup test launcher

--- launch_run_experiments_base.sh
+++ launch_run_experiments_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. /fs/homeu1/eccc/cmd/cmde/olh001/.profile_python3_test
+. /fs/homeu1/eccc/cmd/cmde/olh001/.profile_python3
 
 RDSPS_160_CONFIG_FILE="configs/rdsps/migration_2019_par/rdsps_fc_ops_160_test.cfg"
 
