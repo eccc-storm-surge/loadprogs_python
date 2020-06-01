f921e794f9e5a4da0b772d6cd4040b77d87b9ca5
olh001 <oleksandr.huziy@canada.ca>
2020-06-01 04:44:45 +0000

    fix for debug option

--- src/experiments/run_experiments_base.py
+++ src/experiments/run_experiments_base.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="run experiment")
 
     parser.add_argument("--cfg_paths", nargs="+")
-    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-d", "--debug", action="store_true",
+                         default=False, required=False)
 
     args = parser.parse_args()
 
@@ -25,13 +26,18 @@ if __name__ == '__main__':
 
     if args.debug:
         logger.setLevel(logging.DEBUG)
-    
-    processes = []
 
-    pl = [Process(target=main, kwargs=dict(config_path=Path(p))) for p in cfg_paths]
-    processes.extend(pl)
+    if not args.debug:
+        processes = []
+        pl = [Process(target=main, kwargs=dict(config_path=Path(p))) for p in cfg_paths]
+        processes.extend(pl)
 
-    for p in processes:
-        p.start()
+        for p in processes:
+            p.start()
+    else:
+        for p in cfg_paths:
+            main(config_path=Path(p))
+
+
 
     logger.info(f"Execution time: {time.perf_counter() - t0}")
