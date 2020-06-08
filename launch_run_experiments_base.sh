3a84e691d31599244e35e4c13e00ec8802688d0c
olh001 <oleksandr.huziy@canada.ca>
2020-06-08 13:54:01 +0000

    adjust test launcher

--- launch_run_experiments_base.sh
+++ launch_run_experiments_base.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-. /fs/homeu1/eccc/cmd/cmde/olh001/.profile_python3
+. ~olh001/.profile_python3
 
-RDSPS_160_CONFIG_FILE="configs/rdsps/migration_2019_par/rdsps_fc_ops_160_test.cfg"
+# RDSPS_160_CONFIG_FILE="configs/rdsps/migration_2019_par/rdsps_fc_ops_160_test.cfg"
+RDSPS_160_CONFIG_FILE="configs/rdsps/migration_2019_par/cpop_20191217_new_format/rdsps_fc_ops_160.cfg"
 
 export PYTHONPATH=./src:${PYTHONPATH}
 
-python  ./src/experiments/run_experiments_base.py \
+python  ./src/loadprogs/experiments/run_experiments_base.py \
 --cfg_paths ${RDSPS_160_CONFIG_FILE} \
 --debug
