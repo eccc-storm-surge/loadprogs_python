2b88d7cde099f005ac6a8fc16fbd4fa3bf1b4b03
olh001 <oleksandr.huziy@canada.ca>
2020-08-05 20:16:08 +0000

    loadprogs_python: center the discarded period around the outlier +/- 12h

--- src/loadprogs/data/utils.py
+++ src/loadprogs/data/utils.py
@@ -31,12 +31,11 @@ def remove_spikes(series: pd.Series, whis=1.5, inplace=False):
     # discard the days when outliers occur
     if np.any(eliminate):
         # discard the day with outlier
-        day = pd.Timedelta(days=1)
-        outlier_dates = series_.index[eliminate].floor(day).unique()
+        dt = pd.Timedelta(hours=12)
+        outlier_dates = series_.index[eliminate]
 
         for t in outlier_dates:
-            t2 = t + day
-            series_[(series_.index >= t) & (series_.index < t2)] = np.nan
+            series_[(series_.index >= t - dt) & (series_.index < t + dt)] = np.nan
 
     return series_
 
