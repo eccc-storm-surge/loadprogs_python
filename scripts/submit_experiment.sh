8deb3c7f51f1e74b62b3435ac09c154f3884a0f0
olh001 <oleksandr.huziy@canada.ca>
2020-09-15 15:13:18 +0000

    Add a script for submitting experiments, relevant for big simulations

---
+++ scripts/submit_experiment.sh
@@ -0,0 +1,24 @@
+#ord_soumet= -t 21600 -cpus 40 -cm 160G
+
+# usage:
+#   ord_soumet scripts/submit_experiment.sh -args "=-config configs/gdsps/fc_201904_202006.cfg =-project_root $(true_path .)"
+
+config="i_do_not_exist"
+project_root="i_do_not_exist"
+
+eval $(cclargs $0 "[script to launch verification of different experiments in parallel]"\
+  -project_root "~/Python/loadprogs_python/" "~/Python/loadprogs_python/"  [verification script to be run] \
+  -config "i_do_not_exist" "i_do_not_exist"  [path to the config used by the verification script] \
+  -nosubmit "0" "0" "[put 1 if need to run interactively]" \
+  ++ $*)
+
+
+. ~olh001/.profile_python3
+
+cd ${project_root} || exit
+
+export PYTHONPATH=./src:${PYTHONPATH}
+
+python src/loadprogs/experiments/run_experiments_base.py --cfg ${config}
+
+
