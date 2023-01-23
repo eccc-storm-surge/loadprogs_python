641e9a61ee74fb1cc507c025e55f9435dc8874fe
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2023-01-23 20:06:54 +0000

    Get more info from the test script

--- launch_run_experiments_base.sh
+++ launch_run_experiments_base.sh
@@ -6,6 +6,6 @@ RDSPS_160_CONFIG_FILE="configs/rdsps/migration_2019_par/cpop_20191217_new_format
 
 export PYTHONPATH=./src:${PYTHONPATH}
 
-python  ./src/loadprogs/experiments/run_experiments_base.py \
+/usr/bin/time -v python ./src/loadprogs/experiments/run_experiments_base.py \
 --cfg_paths ${RDSPS_160_CONFIG_FILE} \
 --debug
