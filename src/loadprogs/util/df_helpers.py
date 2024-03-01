400899ae9a9e88701e10de66539771e454ee40dc
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2024-03-01 14:42:10 +0000

    use forward rolling

--- src/loadprogs/util/df_helpers.py
+++ src/loadprogs/util/df_helpers.py
@@ -7,17 +7,11 @@ def apply_rolling(a_df: pd.DataFrame, rolling_period_hours: int,
                   data_column=6) -> pd.DataFrame:
     
     result_db = []
-    prev_chunk = None
     window = pd.Timedelta(hours=rolling_period_hours)
     for do, chunk in a_df.groupby(COLNAME_TORIGIN, sort=True):
         inp = chunk
-        if prev_chunk is not None:
-            inp = pd.concat([inp, prev_chunk[prev_chunk[COLNAME_TIME] < chunk[COLNAME_TIME].min()]])
-
         inp = inp.sort_values(COLNAME_TIME)
-
-        chunk.loc[:, data_column] = inp[[COLNAME_TIME, data_column]].rolling(window, on=COLNAME_TIME).mean().loc[chunk.index, data_column].values
+        chunk.loc[:, data_column] = inp[[COLNAME_TIME, data_column]].iloc[::-1, :].rolling(window, on=COLNAME_TIME).mean().loc[chunk.index, data_column].values
         result_db.append(chunk)
         
-        prev_chunk = chunk
     return pd.concat(result_db, axis=0)
