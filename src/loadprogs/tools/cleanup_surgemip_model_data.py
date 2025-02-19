2999276659aa5a15631ce901d511ff8849bed895
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2025-02-19 18:25:47 +0000

    backup tools from surgemip

--- src/loadprogs/tools/cleanup_surgemip_model_data.py
+++ src/loadprogs/tools/cleanup_surgemip_model_data.py
@@ -2,46 +2,113 @@
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
+
+from convert_gesla_to_loadprogs import exclude_periods
+
+MOD_STNID_COLIDX = 0
+MOD_TIME_COLIDX = 1
+
+STNID_COLNAME = "StnID"
+
+DEFAULT_DATE_FORMAT = r"%Y%m%d%H"
+DATE_FORMATS = [
+    DEFAULT_DATE_FORMAT
+]
+
+def date_parser(t_tok):
+    """Date parser
+
+    Args:
+        t_tok (str): _description_
+
+    Returns:
+        pd.Timestamp: _description_
+    """
+    for fmt in DATE_FORMATS:
+        try:
+            return pd.to_datetime(t_tok, format=fmt)
+        except ValueError:
+            pass
 
 def main():
+    """
+    stations in the station station list are supposed to be ordered the same
+    as in model output files.
+
+    Raises:
+        ValueError: _description_
+    """
     inp_pth = Path("data/model/hourly")
-    out_pth = Path("data/model/clean")
+    out_pth = Path("data/model/clean/hourly")
 
     stnlist_pth = Path("data/obs/stnlist.csv")
 
     out_pth.mkdir(exist_ok=True, parents=True)
 
     df_stn_list = pd.read_csv(stnlist_pth, skiprows=1, header=None)
+    df_stn_list.rename({2: STNID_COLNAME}, axis="columns", inplace=True)
+
+    converters = {
+        MOD_STNID_COLIDX: str, 
+        MOD_TIME_COLIDX: str
+    }
 
     print(df_stn_list.head())
     
     for f_inp in inp_pth.iterdir():
         f_out = out_pth / f_inp.name
-
+        print(f"Cleaning {f_inp} => {f_out}")
         for sep in [",", r"\s+"]:
             try:
-                df_mod = pd.read_csv(f_inp, sep=sep, converters={0: str})
-
-                if len(df_mod.columns) <= 1 or "StnID" not in df_mod:
+                df_mod = pd.read_csv(f_inp, sep=sep, converters=converters)
+                
+                if len(df_mod.columns) <= 1 or STNID_COLNAME not in df_mod:
                     raise ValueError
                 
+                df_mod.iloc[:, MOD_TIME_COLIDX] = pd.to_datetime(df_mod.iloc[:, MOD_TIME_COLIDX], format=DEFAULT_DATE_FORMAT)
 
                 print(df_mod.head())
                 print(df_mod.columns)
 
+                # exclude data for some periods if requested
+                excl = df_mod.iloc[:, MOD_TIME_COLIDX].map(lambda t: False).to_numpy()
 
+                for (t1, t2) in exclude_periods:
+                    excl = excl | df_mod.iloc[:, MOD_TIME_COLIDX].between(t1, t2, inclusive="both").to_numpy()
+
+
+                print(f"Excluding {df_mod.loc[excl, :]}")
+
+                df_mod = df_mod.loc[~excl, :]
+                
+                # remove stations that exist in outputs but not in the selected lisd
+                df_mod = df_mod.loc[df_mod[STNID_COLNAME].isin(df_stn_list[STNID_COLNAME]).to_numpy(), :]
+                df_mod.index = pd.Index(data=range(len(df_mod))) # after removing data the index should be updated
+                
+                
                 n_records_per_station = len(df_mod) / len(df_stn_list)
 
-
+                print(f"{n_records_per_station = }, {len(df_mod) = }, {len(df_stn_list) = }, {(df_mod.index // n_records_per_station).max() = }")
+                print(f"{len(df_stn_list) = }; {df_stn_list.columns = }")
                 print(df_stn_list.iloc[df_mod.index // n_records_per_station, 2])
-                print(df_mod["StnID"])
+                print(df_mod[STNID_COLNAME])
 
-                assert (df_stn_list.iloc[df_mod.index // n_records_per_station, 2].values == df_mod["StnID"].values).all()
+                
+                
+                # make sure the order of stations in the list is the same as in the data files
+                assert np.all(df_stn_list.iloc[df_mod.index // n_records_per_station, 2].values == df_mod[STNID_COLNAME].values)
 
-                df_mod["StnID"] = df_stn_list.iloc[df_mod.index // n_records_per_station, 0].values
+                df_mod[STNID_COLNAME] = df_stn_list.iloc[df_mod.index // n_records_per_station, 0].values
 
                 print(f"Saving clean model data to: {f_out}")
-                df_mod.to_csv(f_out, index=False)
+
+                for (t1, t2) in exclude_periods:
+                    print(f"Checking period {t1} .. {t2}")
+                    count_between = sum(df_mod.iloc[:, MOD_TIME_COLIDX].between(t1, t2))
+                    assert count_between == 0, f"{count_between = }"
+
+                df_mod.to_csv(f_out, index=False, date_format=DEFAULT_DATE_FORMAT)
 
             except ValueError as ve:
                 print(ve)
--- src/loadprogs/tools/convert_gesla_to_loadprogs.py
+++ src/loadprogs/tools/convert_gesla_to_loadprogs.py
@@ -24,16 +24,27 @@ SITE_NAME_TOKEN = "SITE NAME"
 COUNTRY_TOKEN = "COUNTRY"
 NULL_VALUE_TOKEN = "NULL VALUE"
 
+TIME_COLNAME = "time"
 
-def extact_value(token, line, func = lambda x: x):
+# periods to exclude, inclusive
+exclude_periods = (
+    (pd.Timestamp(2017, 9, 1), pd.Timestamp(2017, 12, 31, 23, 59)), 
+)
+
+
+def extract_value(token, line, func = lambda x: x):
     return func(line.split(token)[-1].strip())
 
 
-async def convert_file(inp_f: Path, out_dir: Path):
+async def convert_file(inp_f: Path, out_dir: Path, exclude_periods=()):
     print(f"Converting {inp_f} ...")
 
     stid = inp_f.name
     out_f = out_dir / f"X{stid}.dat"
+    lon = None
+    lat = None
+    site_name = "unknown_site_name"
+    country = "unknown_country"
 
     null_value = None
     # get station coordinates
@@ -50,20 +61,20 @@ async def convert_file(inp_f: Path, out_dir: Path):
                 break
 
             if LON_TOKEN in line:
-                lon = extact_value(LON_TOKEN, line, float)
+                lon = extract_value(LON_TOKEN, line, float)
             elif LAT_TOKEN in line:
-                lat = extact_value(LAT_TOKEN, line, float)
+                lat = extract_value(LAT_TOKEN, line, float)
             elif SITE_NAME_TOKEN in line:
-                site_name = extact_value(SITE_NAME_TOKEN, line)
+                site_name = extract_value(SITE_NAME_TOKEN, line)
             elif COUNTRY_TOKEN in line:
-                country = extact_value(COUNTRY_TOKEN, line)
+                country = extract_value(COUNTRY_TOKEN, line)
             elif NULL_VALUE_TOKEN in line:
-                null_value = extact_value(NULL_VALUE_TOKEN, line, float)
+                null_value = extract_value(NULL_VALUE_TOKEN, line, float)
 
 
     if not out_f.exists():
         df = pd.read_csv(inp_f, 
-                        parse_dates=[[0, 1]], 
+                        parse_dates=[[0, 1]],
                         sep=r"\s+", 
                         comment="#",
                         header=None)
@@ -71,10 +82,21 @@ async def convert_file(inp_f: Path, out_dir: Path):
         df = df.loc[df.iloc[:, QC_FLAG_COLUMN].isin(ACCEPTABLE_QC_VALUES), :]
 
         df = df.loc[df[2] != null_value, :]
+
+        # blacklist requested periods
+        df.rename({"0_1": TIME_COLNAME}, axis="columns", inplace=True)
+        excl = df[TIME_COLNAME].map(lambda t: False)
+
+        for (t1, t2) in exclude_periods:
+            excl = excl | df[TIME_COLNAME].between(t1, t2, inclusive="both")
+            
+        df = df.loc[~excl, :]
         
         df.iloc[:, :2].to_csv(out_f, index=False, 
                             date_format=r"%Y %m %d %H %M",
-                            header=None, sep="\t", float_format=r"%.4f")
+                            header=False, 
+                            sep="\t", 
+                            float_format=r"%.4f")
     else:
         print(f"Exists, won't recreate: {out_f}")
         
@@ -93,10 +115,9 @@ async def main():
     inp_dir = Path("/home/olh001/Python/surgemip/data/obs/GESLA3")
     inp_stn_list_pth = Path("data/obs/stnlist.csv")
     
-    out_dir = inp_dir.parent / f"{inp_dir.name}_loadprogs"
+    out_dir = inp_dir.parent / f"{inp_dir.name}_loadprogs_v4"
     out_obs_file = inp_dir.parent / "gesla3_surgemip.obs"
 
-    
     # ===========
 
     out_dir.mkdir(exist_ok=True, parents=True)
@@ -106,7 +127,7 @@ async def main():
     inp_files = [inp_dir / stid for stid in select_station_ids]
     
     st_info = await asyncio.gather(
-        *[convert_file(inp_f, out_dir) for inp_f in inp_files]
+        *[convert_file(inp_f, out_dir, exclude_periods=exclude_periods) for inp_f in inp_files]
     )
 
     df = pd.DataFrame.from_records(st_info)
