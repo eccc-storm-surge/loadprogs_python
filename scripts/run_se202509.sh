58e10d1d62535242b6521ce9fd6cb76f3f3990fb
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2025-10-16 10:05:49 +0000

    add scripts for GESPS subjective eval 202509

---
+++ scripts/run_se202509.sh
@@ -0,0 +1,34 @@
+#ord_soumet= -mach ppp5 -cpus 80 -t 3600
+
+
+# Example launching for list of dates
+#   for t in $(gen_dates.sh 2025090100 2025090200 24); do 
+#        echo $t; ord_soumet scripts/run_se202509.sh -args $(pwd) ${t:0:-2} 0; 
+#   done
+
+
+. r.load.dot /fs/ssm/eccc/cmd/cmds/fstpy/bundle/2025.8.1 \
+             /fs/ssm/eccc/cmd/cmds/env/python/py310_2023.07.28_all \
+             /fs/ssm/eccc/mrd/rpn/libs/20250804
+
+export PYTHONPATH=./src:${PYTHONPATH}
+
+
+set -u -e
+
+wrk_dir=${1:?"work dir is not set"}
+exp_date=${2:?"exp date is not set"}
+delay_days=${3?"delay days is the third arg"}
+test_run=${4:-1}
+
+exp_date=$(echo ${exp_date} | tr -d ':') # hcron date contains colons
+
+cd ${wrk_dir}
+
+opts=""
+if [ "${test_run}" = "1" ]; then
+    opts="--test-run"
+fi
+
+python src/loadprogs/experiments/se202509.py --date ${exp_date} \
+                                             --delay-days ${delay_days} ${opts}
---
+++ src/loadprogs/experiments/se202509.py
@@ -0,0 +1,109 @@
+
+
+from pathlib import Path
+from loadprogs.main.main import main as loadprogs_main
+from loadprogs.util.constants import OptionNames
+import pandas as pd
+import pytz
+
+import argparse
+
+
+def read_cmd_args():
+    parser = argparse.ArgumentParser(description="subjective evaluation 2025/09")
+
+    
+    parser.add_argument("--date", type=lambda tok: pd.to_datetime(tok, format=r"%Y%m%d", utc=True),
+                        help=r"run date (when the loadprogs is run), in format %Y%m%d",
+                        default=pd.Timestamp.now(tz=pytz.utc))
+
+    parser.add_argument("--delay-days", type=lambda tok: pd.Timedelta(days=int(tok)),
+                        default=pd.Timedelta(days=0),
+                        help="delay in days into the past to select model outputs and obs. If n, look for n days before --date")
+    
+    parser.add_argument("--test-run", action="store_true",
+                        default=pd.Timedelta(days=0),
+                        help="uses TEST_HUB to store results")
+
+    return parser.parse_args()
+
+
+def main():
+    """
+    Select data for subjective evaluation of GESPS against RESPS
+    use one year back of obs data to level.
+    """
+    
+    TEST_HUB = Path("data/se-GESPS-RESPS-202509")
+    args = read_cmd_args()
+
+    # 
+    exp_duration = pd.Timedelta(days=16)
+    exp_hour_list = [0, 12] 
+    obs_duration = pd.Timedelta(days=365) 
+
+    exp_date = args.date - args.delay_days
+
+    common_opts = {
+        OptionNames.obs.OBS_DIR: Path("~swav000/data/ppp5/rarc/CANHYS"),
+        OptionNames.obs.CANHYS_ID_TRANSLATION_DICT: Path("/fs/ssm/eccc/cmd/cmda/ade/external/canhys/1.4.1/cfg/stn.txt"),
+    }
+
+    system_to_opts = {
+        "RESPS": {
+            OptionNames.obs.STATION_INFO: Path("/home/swav000/projets/se-GESPS-RESPS-202509/resps/stations_obs_ATL_1_12.obs"),
+            OptionNames.misc.OUTPUT_TXT: False,
+            OptionNames.misc.OUTPUT_SQLITE: True,
+            OptionNames.mod.DATA_DIR: Path("~swav000/data/ppp5/rarc/RESPS/operation.forecasts.resps"),
+            OptionNames.mod.NOMVAR: "SSH"
+
+        },
+        "GESPS": {
+            OptionNames.obs.STATION_INFO: Path("/home/swav000/projets/se-GESPS-RESPS-202509/gesps/gesps-usr-grid_east-coast-domain.obs"),
+            OptionNames.misc.OUTPUT_TXT: False,
+            OptionNames.misc.OUTPUT_SQLITE: True,
+            OptionNames.mod.DATA_DIR: Path("~swav000/data/ppp5/rarc/GESPS/parallel.forecasts.gesps.usr"),
+            OptionNames.mod.NOMVAR: "SSH",
+            OptionNames.mod.MOD_IP1: 66060288
+        }
+    }
+
+    system_to_cfg_path = {
+        "RESPS": Path("/home/swav000/projets/se-GESPS-RESPS-202509/resps/twl.cfg"),
+        "GESPS": Path("/home/swav000/projets/se-GESPS-RESPS-202509/gesps/twl.cfg")
+    }
+
+
+    for run_hour in exp_hour_list:
+        
+        exp_time = exp_date + pd.Timedelta(hours=run_hour)
+
+        common_opts.update({
+            OptionNames.mod.MOD_BEG_DATE: exp_time,
+            OptionNames.mod.MOD_END_DATE: exp_time,
+            OptionNames.obs.OBS_END_DATE: exp_time + exp_duration,
+            OptionNames.obs.OBS_BEG_DATE: exp_time + exp_duration - obs_duration,
+        })
+
+        for label, overrides in system_to_opts.items():
+            cfg = system_to_cfg_path[label]
+            overrides.update(common_opts)
+
+            if TEST_HUB is not None:
+                out_dir = TEST_HUB / label 
+            else:
+                out_dir = cfg.parent / "data"
+
+            out_dir.mkdir(exist_ok=True, parents=True)
+
+            # create data link near the .cfg files
+            overrides[OptionNames.misc.OUT_FILE_SQLITE] = out_dir / f"{exp_time:%Y%m%d%H}.sqlite"
+            overrides[OptionNames.misc.OUT_DIR] = out_dir
+            overrides[OptionNames.misc.LABEL] = label
+
+            loadprogs_main(config_path=cfg, cfg_overrides=overrides)
+
+
+
+if __name__ == "__main__":
+    main()
